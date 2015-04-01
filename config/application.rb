8fd3945ab4ce8c1b9a9d999cd10be7e668406f65
Yasuhiro Usutani <usutani@tgl.co.jp>
2015-04-01 15:38:01 +0900

    rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Lodge
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = "Tokyo"
     config.autoload_paths += %W(#{config.root}/lib)
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end

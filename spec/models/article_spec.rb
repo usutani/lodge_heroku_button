230f7e98f86c0aa5b54e546bb5b556269dcf54fc
Yasuhiro Usutani <usutani@tgl.co.jp>
2015-04-01 18:34:48 +0900

    Fix create_notification spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -143,7 +143,7 @@ RSpec.describe Article, :type => :model do
     end
 
     it "should create update-notification" do
-      expect(article.create_notification.state).to be_eql(:update)
+      expect(article.create_notification.state.to_sym).to be_eql(:update)
     end
   end
 
